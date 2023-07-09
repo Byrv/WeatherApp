3e6fe6f955c7a45e628796343b83e640d6e42fe5
Byrv <bhyravprasad256@gmail.com>
2023-07-10 01:11:30 +0530

    first commit

---
+++ main/src/components/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '0e27100bb2msh20a747b4196d75dp1d4400jsnd1d54c4e7fdd',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "bd5e378503939ddaee76f12ad7a97608";
